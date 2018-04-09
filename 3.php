b1740ed42df680cc9491fb5ad5f71c47fa523245
Vitalii Yatchenko <vitaliy.yathcenko@gmail.com>
2018-04-09 14:35:42 +0300

    solved myArrayDiff and mySort. Changed myInArray to properly use it in myArrayDiff

--- 3.php
+++ 3.php
@@ -2,7 +2,7 @@
 /*5,6,7,8) Написать свою реализацию следующих функций php: 
 array_sum,  - DONE
 in_array,  - DONE
-array_diff , 
+array_diff, - DONE 
 sort.*/
 class ArrayFuncs
 {
@@ -33,14 +33,14 @@ class ArrayFuncs
 		return $sum;
 	}
 
-	function myInArray ($a)
+	public function myInArray ($a, $arr1)
 	{
 		$i = 0;
 		$j = 0;
-		$count = $this->myCount($this->arr);
+		$count = $this->myCount($arr1);
 		for ($i=0; $i < $count; $i++) 
 		{ 
-			if ($a == $this->arr[$i]) 
+			if ($a == $arr1[$i]) 
 			{
 				$j++;
 			}
@@ -56,11 +56,73 @@ class ArrayFuncs
 			return false;
 		}
 	}
+
+	public function myArrayDiff($arr2)
+	{	
+		//$arr2 - the array to compare from// исходный массив
+		//$this->arr - the array to compare against // с которым идет сравнение
+		$diffAray = [];
+		$count = $this->myCount($this->arr);
+		for ($i=0; $i < $count; $i++) 
+		{
+			if (!($this->myInArray($arr2[$i],$this->arr))) 
+			{
+				if ($this->myInArray($arr2[$i],$diffAray)) 
+				{
+					continue;
+				}
+				else
+				{
+					$diffAray[] = $arr2[$i];
+				}
+			}
+		}
+		return $diffAray;
+	}
+
+	public function mySort()
+	{
+		$count = $this->myCount($this->arr);
+		$transit;
+		$j = 1; // counter for sorted elements
+		while ($j != $count) 
+		{
+			for ($i=0; $i < $count; $i++)
+			{
+				if ($i == $count - 1)  // checks if it is the last element in array
+				{
+					$j++;
+					break 1;
+				}
+				if ($this->arr[$i] > $this->arr[$i+1])  // swaps array elements if needed
+				{
+					$transit = $this->arr[$i];
+					$this->arr[$i] = $this->arr[$i+1];
+					$this->arr[$i+1] = $transit;
+				}
+				else // counts already sorted elements
+				{
+					$j++;
+				}
+			}
+			if ($j != $count) 
+			{
+				$j=0;
+			}
+		}
+		return $this->arr;
+	}
 }
 
 
-$c = [1, 23,4,5];
+
+$c = [4,3,2,6, 10,4, 12,5,1];
+$e = [1,3,3,13,5,4,3,4,3];
 $obj = new ArrayFuncs($c);
 echo $obj->myArraySum();
 echo "<br>";
-var_dump($obj->myInArray(23));
+var_dump($obj->myInArray(23, $c));
+echo "<br>";
+var_dump($obj->myArrayDiff($e));
+echo "<br>";
+var_dump(($obj->mySort()));
